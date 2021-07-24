07f69908902812c18410efd5826162b0acedad58
Shubham Kumar <hello@shubhkumar.in>
2021-07-24 21:00:59 +0530

    Updated Test.

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,6 @@
+const deepEqualInAnyOrder = require('deep-equal-in-any-order');
+const chai = require('chai');
+chai.use(deepEqualInAnyOrder);
 const { expect, assert } = require("chai");
 let request = require("supertest");
 let app = require('../src/app');
@@ -17,9 +20,15 @@ describe('Server', () => {
         })
     })
     describe('Ingest APIs', () => {
-        it.skip('POST /api/v1/ingest', async () => {
-            let res = await request(app).get("/api/v1/ingest");
-            assert.strictEqual(res.status, 200, 'Status is  200');
+        it('POST /api/v1/ingest with Empty body', async () => {
+            let res = await request(app).post("/api/v1/ingest");
+            assert.strictEqual(res.status, 400, 'Status is  400');
+            expect(res.body).to.deep.equalInAnyOrder({ "error": true, "message": "query is mandatory" });
+        })
+        it('POST /api/v1/ingest', async () => {
+            let res = await request(app).post("/api/v1/ingest").send({ "query": "test" });
+            assert.strictEqual(res.status, 202, 'Status is  202');
+            expect(res.body).to.deep.equalInAnyOrder({ "message": "Request Accepted." });
         })
     })
 })
