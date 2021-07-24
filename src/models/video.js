961b0b08fb8b2db05853ab38873514fbb5053b5b
Shubham Kumar <hello@shubhkumar.in>
2021-07-24 20:00:29 +0530

    Added metaData.

--- src/models/video.js
+++ src/models/video.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Video = sequelize.define('Video', {
     title: DataTypes.STRING,
-    description: DataTypes.STRING
+    description: DataTypes.STRING,
+    metaData: DataTypes.JSON
   }, {});
   Video.associate = function (models) {
     // associations can be defined here
