8d649ae3475bcce20607cf2d7feb59feca352bac
Shubham Kumar <hello@shubhkumar.in>
2021-07-24 22:44:45 +0530

    Added Search API.

--- src/routes/video.js
+++ src/routes/video.js
@@ -1,12 +1,34 @@
+const { log } = require('debug');
 var express = require('express');
+const { Op } = require("sequelize");
+const models = require("../models");
 var router = express.Router();
 
 
 /* Search  API */
 /* TODO: Complete This Function */
-router.get('/', function(req, res, next) {
-    res.send({"message":"Fix Me"})
-  });
+router.get('/', async function (req, res, next) {
+  try {
+    let where = req.query.search ?
+      {
+        [Op.or]: [
+          {
+            "title": { [Op.iLike]: "%" + req.query.search + "%" }
+          },
+          {
+            "description": { [Op.iLike]: "%" + req.query.search + "%" }
+          }
+        ]
+      }
+      : {};
+    let videos = await models.Video.findAndCountAll({ where, offset: 0, limit: 10 });
+    return res.json(videos);
+  }
+  catch (err) {
+    return res.status(500).json({ error: true, message: err.message });
+  }
+
+});
 
 
 module.exports = router;
