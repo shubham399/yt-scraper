e908f7d27b3b44ef77febead4020bc5dbcabae4b
Shubham Kumar <hello@shubhkumar.in>
2021-07-25 12:23:47 +0530

    Added Index on DB.

--- src/models/creds.js
+++ src/models/creds.js
@@ -1,8 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Creds = sequelize.define('Creds', {
-    key: DataTypes.STRING, // Will like to encrypt this and store in DB.
-    active: DataTypes.BOOLEAN 
+    key: {
+      type: DataTypes.STRING, // TODO:  Encrypt and store.
+      unique: true,
+    },
+    active: DataTypes.BOOLEAN
   }, {});
   Creds.associate = function (models) {
     // associations can be defined here
--- src/models/video.js
+++ src/models/video.js
@@ -9,7 +9,18 @@ module.exports = (sequelize, DataTypes) => {
     title: DataTypes.STRING,
     description: DataTypes.STRING,
     metaData: DataTypes.JSON
-  }, {});
+  }, {
+    indexes: [{
+      name: 'title_index',
+      using: 'BTREE',
+      fields: ["title"]
+    },
+    {
+      name: 'description_index',
+      using: 'BTREE',
+      fields: ["description"]
+    }]
+  });
   Video.associate = function (models) {
     // associations can be defined here
   };
